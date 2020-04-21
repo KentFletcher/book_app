c9303bc4b24775ebf25703e423ff1f4e25b53204
KentFletcher <kentfletch09@gmail.com>
2020-04-21 10:50:17 -0700

    completed feature one

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
 
-app.get('/hello', renderHome);
+app.get('/', renderHome);
 
 function renderHome (request, response){
   response.render('pages/index');
 }
 
-client.connect()
-  .then(() => {
+// client.connect()
+//   .then(() => {
     app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
-  });
+//   });
