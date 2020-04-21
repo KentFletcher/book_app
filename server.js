ac79b1df6dc218dc537d9012ded354f013bc5de5
KentFletcher <kentfletch09@gmail.com>
2020-04-21 10:39:26 -0700

    completed feature one

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 'use strict';
 
+require('dotenv').config();
+
+const express = require('express');
+const pg = require('pg');
+const superagent = require('superagent');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+const client = new pg.Client(process.env.DATABASE_URL);
+
+app.get('/hello', renderHome);
+
+function renderHome (request, response){
+  response.render('pages/index');
+}
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+  });
