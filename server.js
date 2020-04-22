3d0e1b0695f77522f3394cacd0c6d84dacc9b55c
KentFletcher <kentfletch09@gmail.com>
2020-04-22 11:05:10 -0700

    feature 3 completed rendering to page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 // const client = new pg.Client(process.env.DATABASE_URL);
@@ -19,36 +19,42 @@ app.get('/', renderHome);
 app.get('/searches/new', renderNewSearch);
 app.post('/searches', collectFormData);
 
+
+// Constructor Functions
+function Book(book){
+  this.title = book.title;
+  this.author = book.authors;
+  this.description = book.description;
+  this.isbn = book.isbn;
+  this.bookshelf = book.bookshelf;
+  if(book.imageLinks){
+    this.image = book.imageLinks.thumbnail ? book.imageLinks.thumbnail : url('.styles/img/placeholder_img.png');
+  }
+}
+
+
 // Callback functions
 function renderHome (request, response){
-  response.status(200).render('pages/index');
+  response.status(200).render('./pages/index');
 }
 
 function renderNewSearch (request, response) {
-  response.status(200).render('pages/searches/new');
+  response.status(200).render('./pages/searches/new');
 }
 
 function collectFormData (request, response) {
-  let search = request.body;
-  let searchText = search.searchQuery;
+  let searchText = request.body.searchQuery;
   //   console.log(formData);
-  let radioSelect = search.search;
+  let radioSelect = request.body.search;
   let url = `https://www.googleapis.com/books/v1/volumes?q=+${radioSelect}:${searchText}&maxResult=10`;
 
   superagent.get(url)
-    .then((results) => {
-      console.log('books', results.body.items);
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', { results: results }))
+    .catch(error => {
+      console.log(error);
     });
-
-  response.status(200).render('pages/searches/show');
 }
-
-
-//Constructor Functions
-// function NewBook(book){
-
-// }
-
 
 // client.connect()
 //   .then(() => {
